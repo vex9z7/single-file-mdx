ccc22676ba7fdc09baccf639add87fc9c66b0851
vex9z7 <face.tubas0p@icloud.com>
2025-09-18 17:24:56 +0900

    feat: add MDX support and refactor App into MDX/component structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import mdx from "@mdx-js/rollup";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const viteConfig = defineConfig({
+  plugins: [
+    {
+      enforce: "pre",
+      ...mdx({
+        /* jsxImportSource: …, otherOptions… */
+      }),
+    },
+    react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
+  ],
+});
+
+export default viteConfig;
